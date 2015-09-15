70134a54e4106405ba7f5cf310230aee2ab4f1e0
Miharbi Hernandez <miharbihernandez@gmail.com>
2015-09-15 10:51:57 -0500

    vendor

--- resources/views/partial/navTop.blade.php
+++ resources/views/partial/navTop.blade.php
@@ -4,15 +4,15 @@
 			 <div class="fixed-header">
 				<div class="container">
 					<div class="logo">
-						<a href="/">
-							<img src="images/logo.png" width="266px" height="82px" alt="/">
+						<a href="{{ url('/') }}">
+							<img src="{{ asset('images/logo.png') }}" width="266px" height="82px" alt="/">
 						</a>
 					</div><a href="index.html">
 					<span class="menu"> </span>
-					</a><div class="top-menu"><a href="index.html">
-					</a><nav class="navigation"><a href="index.html">
-						</a><ul class="cl-effect-16"><a href="index.html">
-							</a><li><a href="/"></a><a class="active" href="/">Home</a></li>
+					</a><div class="top-menu"><a href="{{ url('/') }}">
+					</a><nav class="navigation"><a href="{{ url('/') }}">
+						</a><ul class="cl-effect-16"><a href="{{ url('/') }}">
+							</a><li><a href="{{ url('/') }}"></a><a class="active" href="{{ url('/') }}">Home</a></li>
 							<li><a href="#">Nosotras</a></li>
 							<li><a href="#">Staff</a></li>
 							<li><a href="#">Eventos</a></li>
