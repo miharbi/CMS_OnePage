172dd67da9e6a6c000cc4b75cfc3c215de626fba
piainadrianazapiain <ad.nsce@gmail.com>
2015-09-24 19:50:47 -0300

    clases
    nuestras clases

--- resources/views/partial/courses.blade.php
+++ resources/views/partial/courses.blade.php
@@ -1,97 +1,42 @@
-<div class="clases-section">
-			<div class="container">
-			             <h3>Clases</h3>
-			     <div class="class-grids">
-					 <div class="col-md-6 class-grid wow zoomInLeft animated animated" style="visibility: visible; animation-name: zoomInLeft;">
-					     <img src="images/courses/c1.jpg" class="img-responsive" alt="">
-						 <div class="bottom-color">
-							<h4>Aerocombat</h4>
-							<p>Con Alexis Barrera</p>
-						</div>
-						<div class="class-bottom">
-							<div class="col-md-6 class-time ">
-								 <ul class="time">
-									<li><i class="sun"> </i><span>Lunes y Martes</span></li>
-									<li><i class="time"></i><span>16:30</span></li>
-									<div class="clearfix"></div>
-								 </ul>
-							</div>
-							<div class="col-md-6 class-text">
-								<div class="single-one"><span><a href="#"><i class="com"> </i>20l</a></span></div>
-								<div class="single-one"><span><a href="#"><i class="four"> </i>400</a></span></div>
-							</div>
-							<div class="clearfix"></div>
-							  <a class="button" <a="" href="#"><img src="images/read.png" class="img-responsive" alt=""></a>
-						</div>
+<div class="new-section">
+		   <div class="container">
+		         <h3>Nuestras Clases</h3>
+				 <div class="new">
+				   <div class="col-md-6 new-text wow rollIn animated" data-wow-delay="0.4s">
+					   
+					   <a href="single.html"><h4>Zumba</h4></a>
+					   <p>Es una disciplina fitness enfocada por una parte en mantener un cuerpo saludable y por otra en desarrollar, fortalecer y dar flexibilidad al cuerpo mediante movimientos de baile combinados con una serie de rutinas aeróbicas. La zumba utiliza dentro de sus rutinas los principales ritmos latinoamericanos, como lo son la salsa, el merengue, la cumbia, el reggaetón y la samba. En cada sesión de Zumba, se pueden llegar a quemar 1500 calorías.</p>
+				   </div>
+					<div class="col-md-6 welcome-img">
+					 <a href="single.html" class="mask"><img src="images/n1.jpg" alt="image" class="img-responsive zoom-img"></a>
 					</div>
-					 <div class="col-md-6 class-grid wow zoomInRight animated animated" style="visibility: visible; animation-name: zoomInRight;">
-					     <img src="images/courses/c2.jpg" class="img-responsive" alt="">
-						 <div class="bottom-color">
-							<h4>Zumba para niños</h4>
-							<p>Con MAría Josefina</p>
-						</div>
-						<div class="class-bottom">
-							<div class="col-md-6 class-time">
-								 <ul class="time">
-									<li><i class="sun"> </i><span>Miercoles</span></li>
-									<li><i class="time"></i><span>16:00</span></li>
-									<div class="clearfix"></div>
-								 </ul>
-							</div>
-							<div class="col-md-6 class-text">
-								<div class="single-one"><span><a href="#"><i class="com"> </i>20l</a></span></div>
-								<div class="single-one"><span><a href="#"><i class="four"> </i>400</a></span></div>
-							</div>
-							<div class="clearfix"></div>
-							   <a class="button" <a="" href="#"><img src="images/read.png" class="img-responsive" alt=""></a>
-						</div>
+				   <div class="clearfix"> </div>
+			   </div>
+			    <div class="new">
+				 <div class="col-md-6 new-text two wow rollIn animated" data-wow-delay="0.4s">
+					   
+					  <a href="single.html"><h4>Zumba Kids</h4></a>
+										   <p>Ideal para los seguidores de Zumba® más jóvenes. Los niños de entre 7 y 11 años tienen la oportunidad de mantenerse activos y bailar al ritmo de su música favorita.
+Las clases de Zumba® Kids ofrecen rutinas pensadas para niños sobre la base de las coreografías originales de Zumba®. Los pasos se aprenden poco a poco, y agregamos juegos, actividades y elementos de exploración cultural a la estructura de la clase.
+</p>
+				   </div>
+					<div class="col-md-6 new-img two">
+					 <a href="single.html" class="mask"><img src="images/n3.jpg" alt="image" class="img-responsive zoom-img"></a>
 					</div>
-					 <div class="col-md-6 class-grid wow zoomInLeft animated animated" style="visibility: visible; animation-name: zoomInLeft;">
-					     <img src="images/courses/c3.jpg" class="img-responsive" alt="">
-						 <div class="bottom-color">
-							<h4>Zumba</h4>
-							<p>Con Ceci Caman</p>
-						</div>
-						<div class="class-bottom">
-							<div class="col-md-6 class-time">
-								 <ul class="time">
-									<li><i class="sun"> </i><span>Martes y Jueves</span></li>
-									<li><i class="time"></i><span>19:00</span></li>
-									<div class="clearfix"></div>
-								 </ul>
-							</div>
-							<div class="col-md-6 class-text">
-								<div class="single-one"><span><a href="#"><i class="com"> </i>20l</a></span></div>
-								<div class="single-one"><span><a href="#"><i class="four"> </i>400</a></span></div>
-							</div>
-							<div class="clearfix"></div>
-							    <a class="button" href="#"><img src="images/read.png" class="img-responsive" alt=""></a>
-						</div>
+				   <div class="clearfix"> </div>
+			   </div> 
+			    <div class="new">
+				   <div class="col-md-6 new-text wow rollIn animated" data-wow-delay="0.4s">
+					   
+					   <a href="single.html"><h4>Aerocombat</h4></a>
+					   <p>Es una clase de una hora con intensidad al máximo y una variada combinación de golpes de manos y patadas para transpirar y llevar tu energía al 100%.
+Fortalece tus músculos y aumenta tu capacidad aeróbica combinando los movimientos de artes marciales. Trabajarás todo tu cuerpo realizando movimientos de brazos, piernas y desplazamientos en sesiones muy divertidas y con un alto consumo calórico.
+</p>
+				   </div>
+					<div class="col-md-6 welcome-img">
+					 <a href="single.html" class="mask"><img src="images/n2.jpg" alt="image" class="img-responsive zoom-img"></a>
 					</div>
-					 <div class="col-md-6 class-grid wow zoomInRight animated animated" style="visibility: visible; animation-name: zoomInRight;">
-					     <img src="images/courses/c4.jpg" class="img-responsive" alt="">
-						 <div class="bottom-color">
-							<h4>Zumba fitness</h4>
-							<p>Tiare Murcia</p>
-						</div>
-						<div class="class-bottom">
-							<div class="col-md-6 class-time">
-								 <ul class="time">
-									<li><i class="sun"> </i><span>Lunes, Miercoles y Viernes</span></li>
-									<li><i class="time"></i><span>09:15</span></li>
-									<div class="clearfix"></div>
-								 </ul>
-							</div>
-							<div class="col-md-6 class-text">
-								<div class="single-one"><span><a href="#"><i class="com"> </i>20l</a></span></div>
-								<div class="single-one"><span><a href="#"><i class="four"> </i>400</a></span></div>
-
-							</div>
-							<div class="clearfix"></div>
-							  <a class="button" href="#"><img src="images/read.png" class="img-responsive" alt=""></a>
-						</div>
-					</div>
-					<div class="clearfix"></div>
-				 </div>
-			</div>
+				   <div class="clearfix"> </div>
+			   </div>
 		</div>
+	</div>
