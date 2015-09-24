4229c5a5ca41375fe24e3a795cd04873335bbd27
piainadrianazapiain <ad.nsce@gmail.com>
2015-09-24 19:15:51 -0300

    nombres y cargos
    Vision y mision

--- resources/views/partial/us.blade.php
+++ resources/views/partial/us.blade.php
@@ -1,40 +1,48 @@
 <div class="banner-bottom wow bounceIn animated animated" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: bounceIn;">
 	<div class="container">
-	    <h2>Nosotras</h2>
-		<p>Dolor nunc vule putateulr ips dol consec.Donec sem ertet laciniate ultricie upie disse utes comete dolo lectus. fgilla itollicil tua ludin dolor nec met quam accumsan. Dolore condime netus lullam utlacus adipiscing ipsum molestie euismod lore estibulum vel libero ipsum sit
-		amet sollicitudin ante. Aenean imperdiet aliquet hendreritunc interdum ullamcorper lec tuset pellentesqu enim interdum atuspendisse malesuada dignissim.</p>
+	    <h2>Visión y Misión</h2>
+				<p><strong>Misión:</strong> Somos una academia de baile que tiene como objetivo promover la actividad física y la vida sana, a través de 
+					diferentes ritmos.La metodología de las clases está basada en el desarrollo básico de los movimientos, permitiendo que cualquier 
+					persona aprenda a disfrutar el baile de manera rápida y entretenida.
+				</p>
+<p><strong>Visión:</strong> Consolidarnos como una de las mejores academias de baile, de manera transversal, que promoverá la adquisición de hábitos 
+	saludables a través de la calidad, excelencia y compromiso en el servicio de enseñanza dirigido a todos nuestros usuarios.
+</p>
 	 </div>
 </div>
 
-<div class="trainee-section">
+	  <div class="trainee-section">
 		<div class="container">
 			<div class="trainee-grids">
-				<div class="col-md-4 trainee-grid wow zoomInLeft animated animated" style="visibility: visible; animation-name: zoomInLeft;">
-					  <a href="single.html"><img src="images/us/b1.jpg" class="img-responsive" alt=""></a>
-						 <div class="date">
-							 <span>Infinity</span>
-							 <span>Sport</span>
-						 </div>
-					  <a href="single.html"><h4>Karin diaz</h4></a>
+				<div class="col-md-4 trainee-grid wow zoomInLeft animated">
+					  <a href="single.html"><img src="images/b1.jpg" class="img-responsive" alt="" /></a>
+						 
+					  <a href="single.html"><h1><center>Karin diaz</center></h1></a><h4> Gerente de operaciones y Community Manager</h4></a>
 					
 				</div>
-				<div class="col-md-4 trainee-grid wow fadeInUpBig animated animated animated" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUpBig;">
-					  <a href="single.html"><img src="images/us/b2.jpg" class="img-responsive" alt=""></a>
-						<div class="date">
-							<span>Infinity</span>
-							 <span>Sport</span>
-						 </div>
-					 <a href="single.html"><h4>Carolina</h4></a>
+				<div class="col-md-4 trainee-grid wow fadeInUpBig animated animated" data-wow-delay="0.4s">
+					  <a href="single.html"><img src="images/b2.jpg" class="img-responsive" alt="" /></a>
+						
+					  <a href="single.html"><h1><center>Carolina Ovalle</center></h1></a><h4> Gerente Comercial</h4></a>
+					 <h4></h4>
+					 <h4></h4>
 				</div>
-				<div class="col-md-4 trainee-grid wow zoomInRight animated animated" style="visibility: visible; animation-name: zoomInRight;">
-					 <a href="single.html"><img src="images/us/b3.jpg" class="img-responsive" alt=""></a>
-						 <div class="date">
-							<span>Infinity</span>
-							 <span>Sport</span>
-						</div>
-					<a href="single.html"><h4>Fernanda?</h4></a>
+				<div class="col-md-4 trainee-grid wow zoomInRight animated">
+					 <a href="single.html"><img src="images/b3.jpg" class="img-responsive" alt="" /></a>
+						 
+					 <a href="single.html"><h1><center>Fernanda Landaeta</center></h1></a><h4> Directora Ejecutiva</h4></a>
+					 <h4></h4>
+					 <h4></h4>
 				</div>
 				<div class="clearfix"></div>
+				<div class="banner-bottom wow bounceIn animated" data-wow-delay="0.4s">
+			<div class="container">
+			    <h2>Nosotras</h2>
+				<p>Somos tres amigas. Carolina Ing. en Adm. de empresas, joven, libre y alocada. Fernanda, Comunicadora Audiovisual 
+					y productora por esencia. Karin, Comunicadora Audiovisual y Directora de TV. Nos conocemos hace más de 10 años y hemos logrado cultivar y mantener una gran amistad. Hoy hemos dado un gran paso, gracias a la confianza que existe entre nosotras y aprovechando las capacidades de cada una, para unirnos como socias y emprender un nuevo desafío… nuestro querido y gran Infinity Sport.</p>
+
+			 </div>
+		</div>
 		   </div>
 		</div>
 	</div>
