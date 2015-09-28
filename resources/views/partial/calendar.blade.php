a690d64d3565a3b6479a8efebb950ee7f5bd6942
piainadrianazapiain <ad.nsce@gmail.com>
2015-09-28 20:59:59 -0300

    paginas

---
+++ resources/views/partial/calendar.blade.php
@@ -0,0 +1,126 @@
+<div class="clases-section">
+			<div class="container">
+			             <h3>Horarios</h3>
+			     <div class="class-grids">
+					 <div class="col-md-6 class-grid wow zoomInLeft animated">
+					     <img src="images/c1.jpg" class="img-responsive" alt="" />
+						 <div class="bottom-color">
+							<h4>Aerocombat</h4>
+							<p>Con Alexis Barrera</p>
+						</div>
+						<div class="class-bottom">
+							<div class="col-md-6 class-time ">
+								 <ul class="time">
+									<li><i class="sun"> </i><span>Lunes y Martes</span></li>
+									<li><i class="time"></i><span>16:30</span></li>
+									<div class="clearfix"></div>
+								 </ul>
+							</div>
+							<div class="col-md-6 class-text">
+								
+							</div>
+							<div class="clearfix"></div>
+							  
+						</div>
+					</div>
+					 <div class="col-md-6 class-grid wow zoomInRight animated">
+					     <img src="images/c2.jpg" class="img-responsive" alt="" />
+						 <div class="bottom-color">
+							<h4>Zumba para niños</h4>
+							<p>Con MAría Josefina</p>
+						</div>
+						<div class="class-bottom">
+							<div class="col-md-6 class-time">
+								 <ul class="time">
+									<li><i class="sun"> </i><span>Miercoles</span></li>
+									<li><i class="time"></i><span>16:00</span></li>
+									<div class="clearfix"></div>
+								 </ul>
+							</div>
+							
+							<div class="clearfix"></div>
+							   
+						</div>
+					</div>
+					 <div class="col-md-6 class-grid wow zoomInLeft animated">
+					     <img src="images/c3.jpg" class="img-responsive" alt="" />
+						 <div class="bottom-color">
+							<h4>Zumba</h4>
+							<p>Con Ceci Caman</p>
+						</div>
+						<div class="class-bottom">
+							<div class="col-md-6 class-time">
+								 <ul class="time">
+									<li><i class="sun"> </i><span>Martes y Jueves</span></li>
+									<li><i class="time"></i><span>19:00</span></li>
+									<div class="clearfix"></div>
+								 </ul>
+							</div>
+							
+							<div class="clearfix"></div>
+							    
+						</div>
+					</div>
+					 <div class="col-md-6 class-grid wow zoomInRight animated">
+					     <img src="images/c4.jpg" class="img-responsive" alt="" />
+						 <div class="bottom-color">
+							<h4>Zumba fitness</h4>
+							<p>Tiare Murcia</p>
+						</div>
+						<div class="class-bottom">
+							<div class="col-md-6 class-time">
+								 <ul class="time">
+									<li><i class="sun"> </i><span>Lunes, Miercoles y Viernes</span></li>
+									<li><i class="time"></i><span>09:15</span></li>
+									<div class="clearfix"></div>
+								 </ul>
+							</div>
+							
+							<div class="clearfix"></div>
+							  
+						</div>
+					</div>
+					<div class="clearfix"></div>
+					 <div class="col-md-6 class-grid wow zoomInLeft animated">
+					     <img src="images/c5.jpg" class="img-responsive" alt="" />
+						 <div class="bottom-color">
+							<h4>Zumba Fitness</h4>
+							<p>Con Nestor Mateluna</p>
+						</div>
+						<div class="class-bottom">
+							<div class="col-md-6 class-time ">
+								 <ul class="time">
+									<li><i class="sun"> </i><span>Martes y Jueves</span></li>
+									<li><i class="time"></i><span>20:15</span></li>
+									<div class="clearfix"></div>
+								 </ul>
+							</div>
+							<div class="col-md-6 class-text">
+								
+							</div>
+							<div class="clearfix"></div>
+							  
+						</div>
+					</div>
+					 <div class="col-md-6 class-grid wow zoomInRight animated">
+					     <img src="images/c6.jpg" class="img-responsive" alt="" />
+						 <div class="bottom-color">
+							<h4>Zumba</h4>
+							<p>Con Alexis Barrera</p>
+						</div>
+						<div class="class-bottom">
+							<div class="col-md-6 class-time">
+								 <ul class="time">
+									<li><i class="sun"> </i><span>Sabado</span></li>
+									<li><i class="time"></i><span>11:00</span></li>
+									<div class="clearfix"></div>
+								 </ul>
+							</div>
+							
+							<div class="clearfix"></div>
+							   
+						</div>
+					</div>
+				 </div>
+			</div>
+		</div>
