5e3742ca2fed684dae62018390bb21b05d076a77
piainadrianazapiain <ad.nsce@gmail.com>
2015-10-21 20:26:39 -0300

    horario imagen

--- resources/views/partial/schedules.blade.php
+++ resources/views/partial/schedules.blade.php
@@ -4,10 +4,13 @@
 
 		<!--sreen-gallery-cursual-->
 		<div class="col-md-3 tainer wow bounceInLeft" data-wow-delay="0.4s">
+		      
+<img src="images/horario.jpg"</div>
+
+
 		      <h3>Horarios</h3>
 
-		      <img src="images/horario.jpg"
-
+		      
 
 			  
 		</div>
