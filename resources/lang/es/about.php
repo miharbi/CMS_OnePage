be51b018f53a03488112e86c632c486251940889
piainadrianazapiain <ad.nsce@gmail.com>
2015-10-21 21:04:28 -0300

    formulario campos

--- resources/lang/es/about.php
+++ resources/lang/es/about.php
@@ -8,10 +8,10 @@ return [
     'emails' => 'Emails',
     'message' => 'Mensaje',
     'phone_numbers' => 'Telefonos',
-    'name_placeholder' => 'Ingrese su nombre acá...',
-    'email_placeholder' => 'Necesitamos su email para comunicarnos con usted luego',
-    'message_placeholder' => 'Expliquenos en que podemos ayudarle',
-    'contact_us' => 'Contactanos !',
+    'name_placeholder' => 
+    'email_placeholder' => 
+    'message_placeholder' => 
+    'contact_us' => 'Enviar',
     'thanks' => 'Gracias por contactarnos! Pronto recibirás nuestra respuesta',
 
 ];
